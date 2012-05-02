6c795d04bb2d31d9daa6160a5619c8a3dc947bf0
Chris Anderson <jchris@apache.org>
2012-05-02 14:50:54 -0700

    view by timestamp

--- design/views/events/map.js
+++ design/views/events/map.js
@@ -3,11 +3,11 @@ function(doc) {
   if (doc.owa_stream_events) {
     for (i=0; i < doc.owa_stream_events.length; i++) {
       ev = doc.owa_stream_events[i];
-      if (ev.target_url || ev.page_url) {
-        emit(ev.target_url || ev.page_url, ev.timestamp)
+      if (ev.timestamp) {
+        emit(ev.timestamp)
       }
     };
   } else if (doc.owa_target_url || doc.owa_page_url) {
-    emit(doc.owa_target_url || doc.owa_page_url, doc.owa_timestamp)
+    emit(doc.owa_timestamp)
   }
 };
