77b398e1e2f83a46529130ef8621803c2b6dee67
Chris Anderson <jchris@apache.org>
2012-05-03 13:35:12 -0700

    show stats for top 10 urls

--- design/views/url_events/map.js
+++ design/views/url_events/map.js
@@ -4,10 +4,10 @@ function(doc) {
     for (i=0; i < doc.owa_stream_events.length; i++) {
       ev = doc.owa_stream_events[i];
       if (ev.target_url || ev.page_url) {
-        emit(ev.target_url || ev.page_url, ev.timestamp)
+        emit([(ev.target_url || ev.page_url), ev.timestamp])
       }
     };
   } else if (doc.owa_target_url || doc.owa_page_url) {
-    emit(doc.owa_target_url || doc.owa_page_url, doc.owa_timestamp)
+    emit([(doc.owa_target_url || doc.owa_page_url), doc.owa_timestamp])
   }
 };
--- public/js/app.js
+++ public/js/app.js
@@ -6,75 +6,104 @@ $(function() {
     template : Mustache.compile($('#array-keys-values').html())
   })
   
-  coux(["view", "top_urls", {group : true}], function(err, view) {
+  coux(["view", "url_events", {group_level : 1}], function(err, view) {
     view.rows = view.rows.sort(function(a, b) {return b.value - a.value});
-    $('#top-pages').html(Mustache.render($('#keys-values').html(), view))
+    $('#top-pages').html(Mustache.render($('#keys-values').html(), view));
+    
+    var top10 = [];
+    for (var i=0; i < 10; i++) {
+      top10.push(view.rows[i].key[0])
+    };
+    
+    var context = cubism.context()
+    .step(1e3)
+    .size(1000);
+
+    d3.select("#cubism").selectAll(".axis")
+    .data(["top", "bottom"])
+    .enter().append("div")
+    .attr("class", function(d) { return d + " axis"; })
+    .each(function(d) { d3.select(this).call(context.axis().ticks(8).orient(d)); });
+
+    d3.select("#cubism").append("div")
+    .attr("class", "rule")
+    .call(context.rule());
+
+    d3.select("#cubism").selectAll(".horizon")
+    .data([allEvents("events")].concat(top10.map(urlEvents)))
+    .enter().insert("div", ".bottom")
+    .attr("class", "horizon")
+    .call(context.horizon().height(50));
+
+    context.on("focus", function(i) {
+      d3.selectAll(".value").style("right", i == null ? null : context.size() - i + "px");
+    });
+
+    function urlEvents(url) {
+      return context.metric(function(start, stop, step, callback) {
+        start = (+start)/1000, stop = (+stop)/1000, step = (+step)/1000;
+        coux(["view", "url_events", {
+          startkey : [url, start],
+          endkey : [url, stop],
+          group_level : 2
+        }], function(err, view) {
+          if (err) {
+            return callback(err);
+          }
+          var bins = binRows(view.rows, start, step, function(k) {return k[1]});
+          console.log(bins)
+          callback(null, bins);
+        });
+        }, url.split('/').pop());
+    };
+
+
+    function allEvents(name) {
+      return context.metric(function(start, stop, step, callback) {
+        start = (+start)/1000, stop = (+stop)/1000, step = (+step)/1000;
+        coux(["view", "events", {
+          startkey : start,
+          endkey : stop,
+          group : true
+        }], function(err, view) {
+          if (err) {
+            return callback(err);
+          }
+          callback(null, binRows(view.rows, start, step));
+        });
+        }, name);
+      }
+    
   })
   
-  var context = cubism.context()
-  .step(5e3)
-  .size(800);
 
-  d3.select("#cubism").selectAll(".axis")
-  .data(["top"])
-  .enter().append("div")
-  .attr("class", function(d) { return d + " axis"; })
-  .each(function(d) { d3.select(this).call(context.axis().ticks(8).orient(d)); });
 
-  d3.select("#cubism").append("div")
-  .attr("class", "rule")
-  .call(context.rule());
-
-  d3.select("#cubism").selectAll(".horizon")
-  .data([metric("events")])
-  .enter().insert("div", ".bottom")
-  .attr("class", "horizon")
-  .call(context.horizon().extent([0, 50]).height(100));
-
-  context.on("focus", function(i) {
-    d3.selectAll(".value").style("right", i == null ? null : context.size() - i + "px");
-  });
-
-  // Replace this with context.graphite and graphite.metric!
-  function metric(name) {
-    return context.metric(function(start, stop, step, callback) {
-      start = (+start)/1000, stop = (+stop)/1000, step = (+step)/1000;
-      coux(["view", "events", {
-        startkey : start,
-        endkey : stop,
-        group : true
-      }], function(err, view) {
-        if (err) {
-          return callback(err);
+    function binRows(rows, start, step, keyTimeFun) {
+      var times, gap, i, j, val = 0, vals = [];
+      keyTimeFun = keyTimeFun || function(x) {return x};
+      for (i=0; i < rows.length; i++) {
+        rowTime = keyTimeFun(rows[i].key);
+        if (rowTime < start + step) {
+          val += rows[i].value;
+        } else if (rowTime > start + step) {
+          // console.log("val", val)
+          vals.push(val);
+          gap = rowTime - start + step;
+          times = gap / step;
+          // console.log("times", times, start, gap, step, rows[i])
+          for (j=0; j < times; j++) {
+            vals.push(NaN) // nothing for that bin
+          };
+          val = rows[i].value;
+          start += (step * (times+1));
+        } else {
+          // console.log("val", val)
+          vals.push(val);
+          val = rows[i].value;
+          start += step;
         }
-
-        var times, gap, i, j, val = 0; vals = []; rows = view.rows;
-        for (i=0; i < rows.length; i++) {
-          if (rows[i].key < start + step) {
-            val += rows[i].value;
-          } else if (rows[i].key > start + step) {
-            // console.log("val", val)
-            vals.push(val);
-            gap = rows[i].key - start + step;
-            times = gap / step;
-            // console.log("times", times, start, gap, step, rows[i])
-            for (j=0; j < times; j++) {
-              vals.push(NaN) // nothing for that window
-            };
-            val = rows[i].value;
-            start += (step * (times+1));
-          } else {
-            // console.log("val", val)
-            vals.push(val);
-            val = rows[i].value;
-            start += step;
-          }
-        };
-        // console.log("vals", vals)
-        callback(null, vals);
-      });
-      }, name);
+      };
+      return vals;
     }
-
 
 });
